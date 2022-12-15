b1d9f5d42dab4df3a39241377192f21ba6e3376a
hasanuysaal <uysalhasan32@gmail.com>
2022-12-15 11:31:08 +0300

    NoteListViewController created, Notelist screen UI Design created.

---
+++ GameBook/GameBook/Scene/NoteList/Controller/NoteListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NoteListViewController.swift
+//  GameBook
+//
+//  Created by Hasan Uysal on 15.12.2022.
+//
+
+import UIKit
+
+class NoteListViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavBarTitle(view: self, title: "Notes")
+    }
+}
